daca75228262ba72ac8846af143ae92915cf2462
IPINGCHOU <f77252426@gmail.com>
2023-06-18 00:35:19 +0900

    prototype done with progress bar

---
+++ easyParallel.py
@@ -0,0 +1,74 @@
+import multiprocessing
+import time
+from itertools import chain
+
+class ParallelManager():
+    def __init__(self, verbose = False, update_freq = 0.5):
+        self.manager = multiprocessing.Manager()
+        self.queue = self.manager.Queue()
+        self.returns = self.manager.dict()
+        self.verbose = verbose
+        self.update_freq = update_freq
+        # self.lock = self.manager.Lock()
+
+    def do_parallel(self, inputs, task_func, workers, order = True):
+        self.input_len = len(inputs)
+        self.worker_counts = workers
+        self.workers = []
+        self.order = order
+
+        # prepare inputs
+        for i, item in enumerate(inputs):
+            self.queue.put([i, item])
+
+        for w in range(workers):
+            new_worker = multiprocessing.Process(target = self.worker_process, args = (task_func, self.queue))
+            new_worker.start()
+            self.workers.append(new_worker)
+
+        # wait until all process ends
+        while not self.queue.empty():
+            if self.verbose:
+                self.progress_bar_print()
+
+        # make sure all workers ends its work
+        for w in self.workers:
+            w.join()
+
+        # combine results
+        return self.combine_results()
+
+    def worker_process(self, task_func, share_queue):
+
+        while True:
+            try:
+                # get task
+                idx, task = share_queue.get(block = True, timeout = 0.5)
+            except:
+                return 
+
+            # execute
+            self.returns[str(idx)] = task_func(task)
+
+    
+    def edit_array(self, i):
+        return self.returns[int(i)]
+
+    def combine_results(self):
+        
+        # return with orders
+        if self.order:
+            returns = [self.returns[str(i)] for i in range(len(self.returns))]
+            return returns
+        
+        else:
+            return list(chain.from_iterable([*self.returns.values()]))
+        
+    def progress_bar_print(self):
+
+        print('\r Now processed: {0} / {1}, update every {2} sec'.format(
+            len(self.returns),
+            self.input_len, 
+            self.update_freq, end = '\r'))
+        time.sleep(self.update_freq)
+
---
+++ test_parallel.py
@@ -0,0 +1,74 @@
+import pytest
+import easyParallel
+import pandas as pd
+import time
+import math
+
+from functools import reduce
+from collections.abc import MutableMapping
+from random import randint
+
+def test_simpleList():
+    
+    input = [1,2,3,4,5,6,7,8]
+
+    def square(x):
+        return x**2
+    
+    pm = easyParallel.ParallelManager()
+    testa = pm.do_parallel(input, square, 4)
+    print("testa out: ", testa)
+
+    assert testa == [square(i) for i in input]
+
+def test_outputPd():
+    
+    input = [1,2,3,4]
+
+    def give_Series(x):
+        temp = [x, x**2, x**3]
+        return pd.DataFrame([temp],
+                            columns = ["ori", "square", "tri"])
+    
+    pm = easyParallel.ParallelManager()
+    testa = pm.do_parallel(input, give_Series, 4)
+    testa = pd.concat(testa).reset_index(drop = True)
+
+    print("testa out: ", testa)
+    
+    match = [[1,1,1], [2,4,8], [3,9,27], [4,16,64]]
+    match = pd.DataFrame(match,
+                         columns = ["ori", "square", "tri"])
+
+    assert testa.equals(match)
+
+def test_improved():
+
+    def task_func(x):
+        # something takes time
+        randomizer = randint(1000, 10000)
+        for j in range(100):
+            for i in range(randomizer):
+                algo_seed = math.sqrt(math.sqrt(i * randomizer) % randomizer)
+
+        return x
+    
+    input = list(range(200))
+
+    # for loop
+    res1 = []
+    start = time.time()
+    for i in input:
+        res1.append(task_func(i))
+    print("For loops - time used: ", time.time() - start)
+
+    # do_parallel
+    start = time.time()
+    pm = easyParallel.ParallelManager(verbose = True, update_freq = 0.2)
+    print("Init used: ", time.time() - start)
+    testa = pm.do_parallel(input, task_func, 12)
+    print("Parallel - time used: ", time.time() - start)
+
+    assert res1 == testa
+
+
